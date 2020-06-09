50f9592532f1e2a744d14c419b839db797c13989
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-09 07:49:52 +0200

    React Router Dom added

--- ecommerce-clothing/src/App.js
+++ ecommerce-clothing/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import HomePage from "./pages/HomePage/HomePage.component";
 import "./app.scss";
+import { Route, Switch } from "react-router-dom";
 
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>Hats Page</h1>
+		</div>
+	);
+};
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<HomePage />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/hats" component={HatsPage} />
+				</Switch>
 			</div>
 		);
 	}
